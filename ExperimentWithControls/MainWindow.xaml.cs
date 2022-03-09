fa5e4f11547bbbfc07f70db99ec02ff7279c3f3c
Droideight <vincentking700@gmail.com>
2022-03-10 00:42:56 +0800

    FINALIZED

--- ExperimentWithControls/MainWindow.xaml.cs
+++ ExperimentWithControls/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 namespace ExperimentWithControls
 {
     /// <summary>
-    /// MainWindow.xaml 的互動邏輯
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -30,12 +30,7 @@ namespace ExperimentWithControls
             number.Text = numberTextBox.Text;
         }
 
-        private void numberTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
-        {
-            e.Handled = !int.TryParse(e.Text, out int result);
-        }
-
-        private void smallSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             number.Text = smallSlider.Value.ToString("0");
         }
@@ -43,6 +38,39 @@ namespace ExperimentWithControls
         private void bigSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             number.Text = bigSlider.Value.ToString("000-000-0000");
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (sender is RadioButton radioButton)
+            {
+                number.Text = radioButton.Content.ToString();
+            }
+        }
+
+        private void myListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (myListBox.SelectedItem is ListBoxItem listBoxItem)
+            {
+                number.Text = listBoxItem.Content.ToString();
+            }
+        }
+
+        private void readOnlyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (readOnlyComboBox.SelectedItem is ListBoxItem listBoxItem)
+                number.Text = listBoxItem.Content.ToString();
+        }
+
+        private void numberTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !int.TryParse(e.Text, out int result);
+        }
+
+        private void editableComboBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is ComboBox comboBox)
+                number.Text = comboBox.Text;
         }
     }
 }
